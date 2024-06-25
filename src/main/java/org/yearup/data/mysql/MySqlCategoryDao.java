d4781b5d4b552b42f5d4e6fe9533a946897eef29
abbkane97 <kanehaby20@gmail.com>
2024-06-25 11:55:26 -0400

    Category create

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -1,5 +1,6 @@
 package org.yearup.data.mysql;
 
+import org.springframework.jca.cci.connection.ConnectionSpecConnectionFactoryAdapter;
 import org.springframework.stereotype.Component;
 import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
@@ -10,54 +11,81 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
-{
-    public MySqlCategoryDao(DataSource dataSource)
-    {
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
+    public MySqlCategoryDao(DataSource dataSource) {
         super(dataSource);
     }
 
     @Override
-    public List<Category> getAllCategories()
-    {
-        // get all categories
-        return null;
-    }
-        @Override
-        public Category getById(int categoryId)
-        {
-            String sql = "SELECT * FROM products WHERE product_id = ?";
-            try (Connection connection = getConnection())
-            {
-                PreparedStatement statement = connection.prepareStatement(sql);
-                statement.setInt(1, categoryId);
+    public List<Category> getAllCategories() {
+        String sql = "SELECT * FROM categories";
+        List<Category> categories = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
 
-                ResultSet row = statement.executeQuery();
-
-                if (row.next())
-                {
-                    return mapRow(row);
-                }
+            while (resultSet.next()) {
+                Category category = mapRow(resultSet);
+                categories.add(category);
             }
-            catch (SQLException e)
-            {
-                throw new RuntimeException(e);
-            }
-            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error", e);
         }
+        return categories;
 
 
-        // get category by id
-    
+        // get all categories
+    }
 
     @Override
-    public Category create(Category category)
-    {
-        // create a new category
+    public Category getById(int categoryId) {
+        String sql = "SELECT * FROM products WHERE product_id = ?";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, categoryId);
+
+            ResultSet row = statement.executeQuery();
+
+            if (row.next()) {
+                return mapRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
+    }
+
+
+    // get category by id
+
+
+    @Override
+    public Category create(Category category) {
+        String sql = "INSERT INTO categories (name, description) VALUE (?, ?)";
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            statement.executeUpdate();
+
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                int categoryId = generatedKeys.getInt(1);
+                category.setCategoryId(categoryId);
+                return category;
+            } else {
+                throw new SQLException("Creating category failed, no ID obtained.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating category", e);
+        }
+
+        // create a new category
     }
 
     @Override
